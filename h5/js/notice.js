f2f2a5a1ff339fbbc6cb7b9fa17a60da42dc8419
薛帅辉 <534684120@qq.com>
2016-12-08 18:35:53 +0800

    12.8  H5与原生的测试

--- h5/js/notice.js
+++ h5/js/notice.js
@@ -1,14 +1,11 @@
-var parent = $("body");
 function getdata(data){
-    alert(data);
     var data = JSON.parse(data);
-    alert(data)
     if(Number(data.state) == 10000 ){
         //  {"state": 10000,"msg": "成功","result":[{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"},{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"}]}
         var datas = data.result;
         var str = '';
         for( var i in datas){
-            str +='<div class="xsh_noticebox"><a href=""><h3 class="xsh_remind_name">'+(datas[i].title)+'：</h3><p class="xsh_notice_text xsh_remind_text">'+(datas[i].text)+'</p><p class="xsh_notice_text xsh_notice_time">'+(datas[i].time)+'</p></a></div>';
+            str +='<div class="xsh_noticebox"><a href=""><p class="xsh_notice_text">'+(datas[i].title)+'</p> <p class="xsh_notice_text xsh_notice_time">'+(datas[i].time)+'</p> </a></div>';
         }
         parent.append(str);
     }else{
