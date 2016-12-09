9ffe9c0a78e979f742564859f657f3103c43793d
houruishuang <1304820920@qq.com>
2016-12-09 13:38:24 +0800

    获取私信接口

--- app/actions/messageApi.php
+++ app/actions/messageApi.php
@@ -1,10 +1,10 @@
 <?php
+
 require_once 'baseApi.php';
 require_once REPOSITORY.'messageRepository.php';
 
 class messageApi extends baseApi {
     protected $tool;
-
     public function __construct() {
         parent::__construct();
         $this->tool = new messageRepository();
@@ -33,12 +33,32 @@ class messageApi extends baseApi {
         return $this->tool->getPublic($page);
     }
 
-    public function getPersonMessage() {
-
-    }
-
-    public function ignoreMessage() {
-        
+    /**
+     * @SWG\Post(
+     *   path="message-getPm",
+     *   tags={"消息相关"},
+     *   summary="获取私信",
+     *   description="获取私信,获取私信列表和具体私信记录都用此接口",
+     *   operationId="getPm",
+     *   consumes={"application/json"},
+     *   produces={"application/json"},
+     *     @SWG\Parameter(name="uid", in="formData", description="用户ID", required=true, type="string"),
+     *     @SWG\Parameter(name="page", in="formData", description="页码", required=true, type="string"),
+     *     @SWG\Parameter(name="touid", in="formData", description="对方用户ID，没有就不传", required=false, type="string"),
+     *     @SWG\Response(response=200, description="{'state':{结果代码},'result':{返回结果}}"),
+     * )
+     */
+    public function getPm() {
+        $this->checkParam(['uid', 'touid', 'page']);
+        $uid = $this->request->post('uid');
+        $touid = $this->request->post('touid')?:0;
+        $page = $this->request->post('page');
+        $this->tool->blank();
+        $pmList = $this->tool->getPm($uid, $touid, $page);
+        foreach ($pmList as $k=>$value){
+            $pmList[$k]['usericon'] = $this->tool->getAvatar($value['touid']);;
+        }
+        return $pmList;
     }
 
     /**
--- app/repository/messageRepository.php
+++ app/repository/messageRepository.php
@@ -15,4 +15,32 @@ class messageRepository extends baseRepository {
                     ->limit($start.','.$count)
                     ->select();
     }
+
+    public function getPm($uid, $touid, $page, $count = 10) {
+        require_once ROOT.'config/config_ucenter.php';
+        require_once ROOT.'uc_client/client.php';
+        $start = ($page - 1)*$count;
+        $result = [];
+        if(!$touid){
+            $list = uc_pm_list($uid, $start, $count, 'inbox', 'privatepm', 200);
+            $records = $list['data'];
+            foreach ($records as $k=>$record){
+                $result[$k]['touid'] = $record['touid'];
+                $result[$k]['lastupdate'] = $record['lastupdate'];
+                $result[$k]['lastauthor'] = $record['lastauthor'];
+                $result[$k]['lastauthorid'] = $record['lastauthorid'];
+                $result[$k]['message'] = ($record['message']);
+                $result[$k]['isnew'] = $record['isnew'];
+            }
+        }else{
+            $list = uc_pm_view($uid, 0, $touid, 5, $start, $count, 0, 0);
+            foreach ($list as $k=>$record){
+                $result[$k]['touid'] = $record['authorid'];
+                $result[$k]['message'] = $record['message'];
+                $result[$k]['author'] = $record['author'];
+                $result[$k]['position'] = ($record['authorid'] == $uid) ? 'r' : 'l';
+            }
+        }
+        return $result;
+    }
 }
