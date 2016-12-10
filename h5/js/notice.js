88aad0196d9c4212f334ce3f3ce413fd01c03655
薛帅辉 <534684120@qq.com>
2016-12-10 18:31:58 +0800

    12.10 消息-私信详情交互完成

--- h5/js/notice.js
+++ h5/js/notice.js
@@ -1,7 +1,8 @@
+var parent = $("body");
 function getdata(data){
     var data = JSON.parse(data);
     if(Number(data.state) == 10000 ){
-        //  {"state": 10000,"msg": "成功","result":[{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"},{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"}]}
+        //  {"state": 10000,"msg": "成功","result":[{title:"中兴通讯回复",time:"2016-01-25"},{title:"中兴通讯回复",time:"2016-01-25"}]}
         var datas = data.result;
         var str = '';
         for( var i in datas){
@@ -9,6 +10,20 @@ function getdata(data){
         }
         parent.append(str);
     }else{
-        alert(data.msg);
+        window.location.href = "zxbbs://alert/"+data.msg;
+    }
+}
+function reload(data){
+    var data = JSON.parse(data);
+    if(Number(data.state) == 10000 ){
+        //  {"state": 10000,"msg": "成功","result":[{title:"中兴通讯回复",time:"2016-01-25"},{title:"中兴通讯回复",time:"2016-01-25"}]}
+        var datas = data.result;
+        var str = '';
+        for( var i in datas){
+            str +='<div class="xsh_noticebox"><a href=""><p class="xsh_notice_text">'+(datas[i].title)+'</p> <p class="xsh_notice_text xsh_notice_time">'+(datas[i].time)+'</p> </a></div>';
+        }
+        parent.html(str);
+    }else{
+        window.location.href = "zxbbs://alert/"+data.msg;
     }
 }
--- h5/js/remind.js
+++ h5/js/remind.js
@@ -1,17 +1,30 @@
 var parent = $("body");
 function getdata(data){
-    alert(data);
-    //var data = JSON.parse(data);
-    //alert(data)
-    //if(Number(data.state) == 10000 ){
-    //    //  {"state": 10000,"msg": "成功","result":[{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"},{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"}]}
-    //    var datas = data.result;
-    //    var str = '';
-    //    for( var i in datas){
-    //        str +='<div class="xsh_noticebox"><a href=""><h3 class="xsh_remind_name">'+(datas[i].title)+'：</h3><p class="xsh_notice_text xsh_remind_text">'+(datas[i].text)+'</p><p class="xsh_notice_text xsh_notice_time">'+(datas[i].time)+'</p></a></div>';
-    //    }
-    //    parent.append(str);
-    //}else{
-    //    alert(data.msg);
-    //}
+    var data = JSON.parse(data);
+    if(Number(data.state) == 10000 ){
+        //  {"state": 10000,"msg": "成功","result":[{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"},{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"}]}
+        var datas = data.result;
+        var str = '';
+        for( var i in datas){
+            str +='<div class="xsh_noticebox"><a href=""><h3 class="xsh_remind_name">'+(datas[i].title)+'：</h3><p class="xsh_notice_text xsh_remind_text">'+(datas[i].text)+'</p><p class="xsh_notice_text xsh_notice_time">'+(datas[i].time)+'</p></a></div>';
+        }
+        parent.append(str);
+    }else{
+        window.location.href = "zxbbs://alert/"+data.msg;
+    }
+}
+
+function reload(data){
+    var data = JSON.parse(data);
+    if(Number(data.state) == 10000 ){
+        //  {"state": 10000,"msg": "成功","result":[{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"},{title:"中兴通讯回复",text:"中兴通讯中兴通讯中兴通讯中兴通讯",time:"2016-01-25"}]}
+        var datas = data.result;
+        var str = '';
+        for( var i in datas){
+            str +='<div class="xsh_noticebox"><a href=""><h3 class="xsh_remind_name">'+(datas[i].title)+'：</h3><p class="xsh_notice_text xsh_remind_text">'+(datas[i].text)+'</p><p class="xsh_notice_text xsh_notice_time">'+(datas[i].time)+'</p></a></div>';
+        }
+        parent.html(str);
+    }else{
+        window.location.href = "zxbbs://alert/"+data.msg;
+    }
 }
