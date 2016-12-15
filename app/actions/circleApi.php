69c30f2eba30db83b954579194a3355704729036
houruishuang <1304820920@qq.com>
2016-12-15 15:23:48 +0800

    founder

--- app/actions/circleApi.php
+++ app/actions/circleApi.php
@@ -241,7 +241,7 @@ class circleApi extends baseApi {
         $count = $sCount ?: 10;
         $level = $level == 5 ? 0 : $level;
         $users = $this->tool->getGroupUser($fid, 'uid, username', $page, $count, $level);
-        if($level){
+        if($level != 5){
             $groupProfile = $this->tool->getGroupProfile($fid);
             array_push($users, ['uid'=>$groupProfile['founderuid'], 'username'=>$groupProfile['foundername']]);
         }
