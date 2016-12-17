996122e30772a53b26f21e0e00269ae7d353d378
houruishuang <1304820920@qq.com>
2016-12-17 18:55:32 +0800

    置顶

--- app/repository/indexRepository.php
+++ app/repository/indexRepository.php
@@ -42,9 +42,10 @@ class indexRepository extends baseRepository {
         return $this->table('forum_threadtype')->fields('typeid, name')->order('displayorder')->select();
     }
 
-    public function getInfo($typeId, $page) {
+    public function getInfo($typeId, $page = 1, $count = 10) {
         $count = $this->table('common_setting')->where('skey', 'index_list_count')->find();
-        $datas = $this->table('forum_thread')->ass('zt')->join(' LEFT JOIN '.$this->prefix.'forum_post AS tz ON zt.tid = tz.tid')->where(['zt.sortid'=>$typeId, 'tz.first'=>1])->select();
+        $start = ($page - 1) * $count;
+        $datas = $this->table('forum_thread')->ass('zt')->join(' LEFT JOIN '.$this->prefix.'forum_post AS tz ON zt.tid = tz.tid')->where(['zt.sortid'=>$typeId, 'tz.first'=>1])->limit($start.','.$count)->select();
         $valueAble = [];
         foreach ($datas as $k=>$data) {
             $valueAble[$k]['title'] = $this->subString($data['subject'], 25);
