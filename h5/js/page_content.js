0d11268c358fcd660c5abf5056edf5a1bcb88d83
薛帅辉 <534684120@qq.com>
2016-12-17 11:52:50 +0800

    12.15 图片预览事件绑定

--- h5/js/page_content.js
+++ h5/js/page_content.js
@@ -32,7 +32,7 @@ function replydata(result){
         var data = results.result;
         for(var i in data){
             var ziliao = http+'/app.php?show=member-details&uid='+data[i].authorid;
-            var str='<li class="xsh_floor" pid="'+(data[i].pid)+'"><a href="zxbbs://jump/'+(escape(ziliao.replace(/\//g,"##")))+'" uid="'+(data[i].authorid)+'"><img src="'+(data[i].usericon)+'" class="xsh_user_logo xsh_user_logo_radius xsh_post_user_logo"></a><p><span class="xsh_floor_username">'+(data[i].author)+'：</span><span class="xsh_floor_number">'+(Number(result.position)-1)+'</span></p><div class="xsh_floor_textbox"><div class="reply"><p>'+(data[i].reply.split("\n")[0])+'</p><p>'+(data[i].reply.split("\n")[1])+'</p></div><p class="xsh_floor_text">'+(data[i].message)+'</p><ul>';
+            var str='<li class="xsh_floor" pid="'+(data[i].pid)+'"><a href="zxbbs://jump/'+(escape(ziliao.replace(/\//g,"##")))+'" uid="'+(data[i].authorid)+'"><img src="'+(data[i].usericon)+'" class="xsh_user_logo xsh_user_logo_radius xsh_post_user_logo"></a><p><span class="xsh_floor_username">'+(data[i].author)+'：</span><span class="xsh_floor_number">'+(Number(data.position)-1)+'</span></p><div class="xsh_floor_textbox"><div class="reply"><p>'+(data[i].reply.split("\n")[0])+'</p><p>'+(data[i].reply.split("\n")[1])+'</p></div><p class="xsh_floor_text">'+(data[i].message)+'</p><ul>';
             var images=[];
             var att = data[i].attach;
             for (var j in att){
@@ -100,7 +100,7 @@ function getdata(results){
         var data = results.result;
         for(var i in data){
             var ziliao = http+'/app.php?show=member-details&uid='+data[i].authorid;
-            var str='<li class="xsh_floor" pid="'+(data[i].pid)+'"><a href="zxbbs://jump/'+(escape(ziliao.replace(/\//g,"##")))+'" uid="'+(data[i].authorid)+'"><img src="'+(data[i].usericon)+'" class="xsh_user_logo xsh_user_logo_radius xsh_post_user_logo"></a><p><span class="xsh_floor_username">'+(data[i].author)+'：</span><span class="xsh_floor_number">'+(Number(result.position)-1)+'</span></p><div class="xsh_floor_textbox"><div class="reply"><p>'+(data[i].reply.split("\n")[0])+'</p><p>'+(data[i].reply.split("\n")[1])+'</p></div><p class="xsh_floor_text">'+(data[i].message)+'</p><ul>';
+            var str='<li class="xsh_floor" pid="'+(data[i].pid)+'"><a href="zxbbs://jump/'+(escape(ziliao.replace(/\//g,"##")))+'" uid="'+(data[i].authorid)+'"><img src="'+(data[i].usericon)+'" class="xsh_user_logo xsh_user_logo_radius xsh_post_user_logo"></a><p><span class="xsh_floor_username">'+(data[i].author)+'：</span><span class="xsh_floor_number">'+(Number(data.position)-1)+'</span></p><div class="xsh_floor_textbox"><div class="reply"><p>'+(data[i].reply.split("\n")[0])+'</p><p>'+(data[i].reply.split("\n")[1])+'</p></div><p class="xsh_floor_text">'+(data[i].message)+'</p><ul>';
             var images=[];
             var att = data[i].attach;
             for (var j in att){
