5d1c24d25c15026af263b56739b8185f067deaae
薛帅辉 <534684120@qq.com>
2016-12-17 19:43:36 +0800

    12.15  test

--- h5/js/page_content.js
+++ h5/js/page_content.js
@@ -27,11 +27,11 @@ function gettime(time){
 /*回复的数据*/
 function replydata(result){
     var results = JSON.parse(result);
-    alert(result);
         /*插入到1楼前*/
         var data = results.result;
         var str ='';
             var ziliao = http+'/app.php?show=member-details&uid='+uid;
+    alert(data);
             str +='<li class="xsh_floor" pid="'+(data.pid)+'"><a name="'+(uid)+'" href="zxbbs://jump/'+(escape(ziliao.replace(/\//g,"##")))+'" uid="'+(data.authorid)+'"><img src="'+(data.usericon)+'" class="xsh_user_logo xsh_user_logo_radius xsh_post_user_logo"></a><p><span class="xsh_floor_username">'+(data.author)+'：</span><span class="xsh_floor_number"></span></p><div class="xsh_floor_textbox">';
             if(data.reply){
                 str +='<div class="reply"><p>'+(data.reply.split("\n")[0])+'</p><p>'+(data.reply.split("\n")[1])+'</p></div>';
