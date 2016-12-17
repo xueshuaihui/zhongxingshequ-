ed5eecd4d0c6e93c77a1be518cac055774cab095
薛帅辉 <534684120@qq.com>
2016-12-17 20:01:14 +0800

    12.15  test

--- h5/js/page_content.js
+++ h5/js/page_content.js
@@ -26,11 +26,11 @@ function gettime(time){
 }
 /*回复的数据*/
 function replydata(result){
+    alert(JSON.parse(result))
         var results = JSON.parse(result);
         /*插入到1楼前*/
     alert(results);
         var data = results.result;
-    alert(data)
         var str = '';
         var ziliao = http+'/app.php?show=member-details&uid='+uid;
         str +='<li class="xsh_floor" pid="'+(data.pid)+'"><a name="'+(uid)+'" href="zxbbs://jump/'+(escape(ziliao.replace(/\//g,"##")))+'" uid="'+(data.authorid)+'"><img src="'+(data.usericon)+'" class="xsh_user_logo xsh_user_logo_radius xsh_post_user_logo"></a><p><span class="xsh_floor_username">'+(data.author)+'：</span><span class="xsh_floor_number"></span></p><div class="xsh_floor_textbox">';
@@ -38,7 +38,6 @@ function replydata(result){
             str +='<div class="reply"><p>'+(data.reply.split("\n")[0])+'</p><p>'+(data.reply.split("\n")[1])+'</p></div>';
         }
             str +='<p class="xsh_floor_text">'+(data.message)+'</p><ul>';
-        alert(str);
         var images=[];
         var att = data.attach;
         for (var j in att){
