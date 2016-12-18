7091981467d056f6ae2577c6153f620b995c7569
houruishuang <1304820920@qq.com>
2016-12-18 14:44:48 +0800

    全部圈子列表

--- app/repository/pageRepository.php
+++ app/repository/pageRepository.php
@@ -38,7 +38,7 @@ class pageRepository extends baseRepository {
         $threadDatas = $data->select('tid, fid, author, authorid, subject, lastpost, digest, highlight, bgcolor, stamp, displayorder');
         foreach ($threadDatas as $k=>$threadData) {
             $threadClassData = $this->table('forum_threadclass')->where(['fid'=>$fid, 'typeid'=>$threadData['typeid']])->find();
-            $threadDatas[$k] = array_merge($threadDatas[$k], $threadClassData);
+            $threadDatas[$k]['name'] = $threadClassData['name'];
         }
         return $threadDatas;
     }
